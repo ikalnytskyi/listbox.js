04a3fa5b1e031c8290305b1fe4a55d81319f0799
Igor Kalnitsky <igor@kalnitsky.org>
2013-10-07 00:31:20 +0300

    More unit tests!

--- tests/tests.js
+++ tests/tests.js
@@ -1,40 +1,420 @@
 // ==================================================================
-//  SingleSelectListbox TestCases
+//  Helpers
+// ==================================================================
+
+function ms_ItemsToVal(items) {
+  var result = '';
+  for (var i = 0; i < items.length; ++i) {
+    if (i != 0)
+      result += ',';
+    result += $(items[i]).text();
+  }
+  return result;
+}
+
+
+// ==================================================================
+//  SingleSelectListbox Test Cases
 // ==================================================================
 
 module('SingleSelectListbox');
 
 
 test('construct default', function () {
-  var select = $('#construct-default');
+  var select = $('#test').listbox();
 
-  // check for exists a fake listbox
   var listbox = select.next();
   equal(listbox.attr('class'), 'lbjs');
 
-  // check for non exists a searchbar
   var searchbar = listbox.find('.lbjs-searchbar');
   notEqual(searchbar.attr('class'), 'lbjs-searchbar');
 
-  // check for exists a fake list
   var list = listbox.find('.lbjs-list');
   equal(list.attr('class'), 'lbjs-list');
 });
 
 
 test('construct with searchbar', function () {
-  var select = $('#construct-with-searchbar');
+  var select = $('#test').listbox({'searchbar': true});
+  var listbox = select.next();
 
-  // check for exists a searchbar
-  var searchbar = select.next().find('.lbjs-searchbar');
+  var searchbar = listbox.find('.lbjs-searchbar');
   equal(searchbar.attr('class'), 'lbjs-searchbar');
 });
 
 
 test('construct with class', function () {
-  var select = $('#construct-with-class');
+  var select = $('#test').listbox({'class': 'testClass'});
+  var listbox = select.next();
 
-  // check for exists a fake listbox
+  equal(listbox.attr('class'), 'lbjs testClass');
+});
+
+
+test('implicit default value', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+    .listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var selectedItems = list.children('[selected]');
+
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'A');
+  equal(selectedItems.text(), select.val());
+});
+
+
+test('explicit default value', function () {
+  var select = $('#test')
+    .append('<option         >A</option>')
+    .append('<option selected>B</option>')
+    .append('<option         >C</option>')
+    .listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var selectedItems = list.children('[selected]');
+
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'B');
+  equal(selectedItems.text(), select.val());
+});
+
+
+test('two explicit default values', function () {
+  var select = $('#test')
+    .append('<option         >A</option>')
+    .append('<option selected>B</option>')
+    .append('<option selected>C</option>')
+    .listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var selectedItems = list.children('[selected]');
+
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'C');
+  equal(selectedItems.text(), select.val());
+});
+
+
+test('one click', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+    .listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var items = list.children();
+
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'B');
+  equal(selectedItems.text(), select.val());
+});
+
+
+test('multiple clicks', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+    .listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var items = list.children();
+
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'B');
+  equal(selectedItems.text(), select.val());
+
+  items[2].click();     // click on 'C'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'C');
+  equal(selectedItems.text(), select.val());
+
+  items[0].click();     // click on 'A'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'A');
+  equal(selectedItems.text(), select.val());
+
+  items[0].click();     // click on 'A'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'A');
+  equal(selectedItems.text(), select.val());
+
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'B');
+  equal(selectedItems.text(), select.val());
+
+  items[2].click();     // click on 'C'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'C');
+  equal(selectedItems.text(), select.val());
+});
+
+
+
+
+// ==================================================================
+//  MultiSelectListbox Test Cases
+// ==================================================================
+
+module('MultiSelectListbox');
+
+
+test('construct default', function () {
+  var select = $('#test')
+    .attr('multiple', '')
+    .listbox()
+  ;
+
+  var listbox = select.next();
+  equal(listbox.attr('class'), 'lbjs');
+
+  var searchbar = listbox.find('.lbjs-searchbar');
+  notEqual(searchbar.attr('class'), 'lbjs-searchbar');
+
+  var list = listbox.find('.lbjs-list');
+  equal(list.attr('class'), 'lbjs-list');
+});
+
+
+test('construct with searchbar', function () {
+  var select = $('#test')
+    .attr('multiple', '')
+    .listbox({'searchbar': true})
+  ;
+
+  var listbox = select.next();
+  var searchbar = listbox.find('.lbjs-searchbar');
+
+  equal(searchbar.attr('class'), 'lbjs-searchbar');
+});
+
+
+test('construct with class', function () {
+  var select = $('#test')
+    .attr('multiple', '')
+    .listbox({'class': 'testClass'})
+  ;
+
   var listbox = select.next();
   equal(listbox.attr('class'), 'lbjs testClass');
+});
+
+
+test('implicit default value', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+
+    .attr('multiple', '').listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var selectedItems = list.children('[selected]');
+
+  equal(selectedItems.length, 0);
+});
+
+
+test('explicit default value', function () {
+  var select = $('#test')
+    .append('<option         >A</option>')
+    .append('<option selected>B</option>')
+    .append('<option         >C</option>')
+
+    .attr('multiple', '').listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var selectedItems = list.children('[selected]');
+
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'B');
+  equal(selectedItems.text(), select.val());
+});
+
+
+test('two explicit default values', function () {
+  var select = $('#test')
+    .append('<option         >A</option>')
+    .append('<option selected>B</option>')
+    .append('<option selected>C</option>')
+
+    .attr('multiple', '').listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var selectedItems = list.children('[selected]');
+
+  equal(selectedItems.length, 2);
+  equal(ms_ItemsToVal(selectedItems), 'B,C');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+});
+
+
+test('one click', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+
+    .attr('multiple', '').listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var items = list.children();
+
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(selectedItems.text(), 'B');
+  equal(selectedItems.text(), select.val());
+});
+
+
+test('two clicks', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+
+    .attr('multiple', '').listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var items = list.children();
+
+  items[1].click();     // click on 'B'
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 0);
+  equal(select.val(), null);
+});
+
+
+test('two clicks on different items', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+
+    .attr('multiple', '').listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var items = list.children();
+
+  items[0].click();     // click on 'A'
+  items[2].click();     // click on 'C'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 2);
+  equal(ms_ItemsToVal(selectedItems), 'A,C');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+});
+
+
+test('multiple clicks', function () {
+  var select = $('#test')
+    .append('<option>A</option>')
+    .append('<option>B</option>')
+    .append('<option>C</option>')
+
+    .attr('multiple', '').listbox()
+  ;
+
+  var list = select.next().find('.lbjs-list');
+  var items = list.children();
+
+  items[0].click();     // click on 'A'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(ms_ItemsToVal(selectedItems), 'A');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 2);
+  equal(ms_ItemsToVal(selectedItems), 'A,B');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[0].click();     // click on 'A'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(ms_ItemsToVal(selectedItems), 'B');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[2].click();     // click on 'C'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 2);
+  equal(ms_ItemsToVal(selectedItems), 'B,C');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[0].click();     // click on 'A'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 3);
+  equal(ms_ItemsToVal(selectedItems), 'A,B,C');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 2);
+  equal(ms_ItemsToVal(selectedItems), 'A,C');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[0].click();     // click on 'A'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(ms_ItemsToVal(selectedItems), 'C');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[1].click();     // click on 'B'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 2);
+  equal(ms_ItemsToVal(selectedItems), 'B,C');
+  equal(ms_ItemsToVal(selectedItems), select.val());
+
+  items[2].click();     // click on 'C'
+
+  var selectedItems = list.children('[selected]');
+  equal(selectedItems.length, 1);
+  equal(ms_ItemsToVal(selectedItems), 'B');
+  equal(ms_ItemsToVal(selectedItems), select.val());
 });
