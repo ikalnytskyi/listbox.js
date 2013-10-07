0484cc941961d4b5d49fed87be7e990a8db97612
Igor Kalnitsky <igor@kalnitsky.org>
2013-10-07 16:34:45 +0300

    Fixed syntax errors in tests.js.
    JavaScript is so terrible that Chrome eat this code good, but another
    browser fail.

--- tests/tests.js
+++ tests/tests.js
@@ -113,7 +113,7 @@ test('one click', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
@@ -133,42 +133,42 @@ test('multiple clicks', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(selectedItems.text(), 'B');
   equal(selectedItems.text(), select.val());
 
-  items[2].click();     // click on 'C'
+  $(items[2]).click();     // click on 'C'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(selectedItems.text(), 'C');
   equal(selectedItems.text(), select.val());
 
-  items[0].click();     // click on 'A'
+  $(items[0]).click();     // click on 'A'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(selectedItems.text(), 'A');
   equal(selectedItems.text(), select.val());
 
-  items[0].click();     // click on 'A'
+  $(items[0]).click();     // click on 'A'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(selectedItems.text(), 'A');
   equal(selectedItems.text(), select.val());
 
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(selectedItems.text(), 'B');
   equal(selectedItems.text(), select.val());
 
-  items[2].click();     // click on 'C'
+  $(items[2]).click();     // click on 'C'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
@@ -193,11 +193,11 @@ test('change event', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[0].click();
+  $(items[0]).click();
   equal(receiveCounter, 1);
 
-  items[1].click();
-  items[2].click();
+  $(items[1]).click();
+  $(items[2]).click();
 
   equal(receiveCounter, 3);
 });
@@ -315,7 +315,7 @@ test('one click', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
@@ -336,8 +336,8 @@ test('two clicks', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[1].click();     // click on 'B'
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 0);
@@ -357,8 +357,8 @@ test('two clicks on different items', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[0].click();     // click on 'A'
-  items[2].click();     // click on 'C'
+  $(items[0]).click();     // click on 'A'
+  $(items[2]).click();     // click on 'C'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 2);
@@ -379,63 +379,63 @@ test('multiple clicks', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[0].click();     // click on 'A'
+  $(items[0]).click();     // click on 'A'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(ms_ItemsToVal(selectedItems), 'A');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 2);
   equal(ms_ItemsToVal(selectedItems), 'A,B');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[0].click();     // click on 'A'
+  $(items[0]).click();     // click on 'A'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(ms_ItemsToVal(selectedItems), 'B');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[2].click();     // click on 'C'
+  $(items[2]).click();     // click on 'C'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 2);
   equal(ms_ItemsToVal(selectedItems), 'B,C');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[0].click();     // click on 'A'
+  $(items[0]).click();     // click on 'A'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 3);
   equal(ms_ItemsToVal(selectedItems), 'A,B,C');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 2);
   equal(ms_ItemsToVal(selectedItems), 'A,C');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[0].click();     // click on 'A'
+  $(items[0]).click();     // click on 'A'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
   equal(ms_ItemsToVal(selectedItems), 'C');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[1].click();     // click on 'B'
+  $(items[1]).click();     // click on 'B'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 2);
   equal(ms_ItemsToVal(selectedItems), 'B,C');
   equal(ms_ItemsToVal(selectedItems), select.val());
 
-  items[2].click();     // click on 'C'
+  $(items[2]).click();     // click on 'C'
 
   var selectedItems = list.children('[selected]');
   equal(selectedItems.length, 1);
@@ -461,11 +461,11 @@ test('change event', function () {
   var list = select.next().find('.lbjs-list');
   var items = list.children();
 
-  items[0].click();
+  $(items[0]).click();
   equal(receiveCounter, 1);
 
-  items[1].click();
-  items[2].click();
+  $(items[1]).click();
+  $(items[2]).click();
 
   equal(receiveCounter, 3);
 });
